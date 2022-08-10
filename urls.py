8b770915d59c3fe6fe9106f2a7986224f716d684
devjolt <tom.m.warburton@gmail.com>
2022-08-10 21:43:07 +0100

    major changes to joltfolio

--- urls.py
+++ urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.joltfolio, name='joltnet'),
     path('tang/', views.home, name='home'),
+    path('joltfolio2/', views.joltfolio2, name='joltfolio2'),
+    path('generic/', views.joltfolioGeneric, name='joltfolioGeneric'),
     #path('target_range/', views.target_range, name='target_range'),
     #path('portfolio/', views.portfolio, name='portfolio'),
 ]
--- views.py
+++ views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'justajolt_home/home.html')
 
-
 def target_range(request):
     return render(request, 'justajolt_home/target_range.html')
 
 def joltfolio(request):
-    return render(request, 'justajolt_home/joltfolio.html')
+    return render(request, 'justajolt_home/joltfolioMultiBoot.html')
+
+def joltfolio2(request):
+    return render(request, 'justajolt_home/joltfolio2.html')
     
 def portfolio(request):
     return render(request, 'justajolt_home/portfolio.html')
 
+def joltfolioGeneric(request):
+    return render(request, 'justajolt_home/joltfolio1Generic.html')
